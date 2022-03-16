1ef09789a10b6a8abc1b3f5c886f039380a6d438
Quentin Ordener <quentin.ordener@bnc.ca>
2022-03-16 11:34:39 -0400

    Finish project

--- App/index.tsx
+++ App/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import Navigation from './config/Navigation';
+import { api } from './util/api';
+
+api('/latest?base=USD')
+  .then((res) => {
+    console.log('response', res);
+  })
+  .catch((error) => {
+    console.log('error', error);
+  });
 
 const index = () => <Navigation />;
 
